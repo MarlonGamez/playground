6a7e979153166882d4d484376d5224166b78067c
Marlon Gamez <marlongamez98@gmail.com>
2021-04-24 13:51:18 -0700

    add basic error handling with macros

--- playground/src/Application.cpp
+++ playground/src/Application.cpp
@@ -8,6 +8,27 @@
 #include <fstream>
 #include <algorithm>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__));
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 enum ShaderType { vertex, fragment };
 
 GLuint LoadSingleShader(const char* shaderFilePath, ShaderType type)
@@ -175,7 +196,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
